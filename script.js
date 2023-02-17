81dd5f3472a3f9c378a58859d9f2266a91f26a0e
Lucas Bispo <skaliboo1@gmail.com>
2023-02-16 21:40:13 -0300

    add slide, script and css

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+const totalImagsn = document.querySelectorAll('.box-image').length - 1;
+let interval = 0
+
+
+const action = () => {
+    let img = document.querySelectorAll('.box-image img');
+    const bar = document.querySelectorAll('.bar');
+    setInterval(() => {
+        img[interval].style.display = 'none';
+        interval++;
+        if (interval > totalImagsn) {
+            interval = 0;
+        }
+        img[interval].style.display = 'block';
+    }, 3000);
+}
+
+action();
